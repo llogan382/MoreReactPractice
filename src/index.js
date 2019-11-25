0474fe8821a79c05b87c92a1047968604d7baace
Luke Logan <llogan382@gmail.com>
2019-11-25 11:30:42 -0500

    Format conditionally

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ function formatName(user) {
     return user.firstName + ' ' + user.lastName;
 }
 
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>
+    }
+    return <h1>Hello, Stranger!</h1>
+}
+
 const user = {
     firstName: 'Taylor',
     lastName: 'Register'
@@ -15,7 +22,7 @@ const user = {
 
 const element = (
     <h1>
-        Hello, {formatName(user)}!
+        {getGreeting(user)}!
     </h1>
 );
 
