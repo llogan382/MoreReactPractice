c72b200dae3dff0a459b84f23ffeb8aa82df50b3
Luke Logan <llogan382@gmail.com>
2019-11-25 11:58:32 -0500

    Calling multiple components

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 
-class Welcome extends React.Component {
-    render() {
-        return <h1>Hello, {this.props.name}</h1>;
-    }
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
 }
 
-const element = <Welcome name="Sara" />;
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    element,
+    <App />,
     document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
